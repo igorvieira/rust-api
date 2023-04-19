70a72c6418b5b181c2a366ae5583a18d8f77ce80
Igor Vieira <igor.p.r.vieira@gmail.com>
2023-04-18 21:59:05 -0300

    chore: add function to edit and get task by id

--- src/services.rs
+++ src/services.rs
@@ -1,6 +1,6 @@
 use crate::{
     model::TaskModel,
-    schema::{ CreateTaskSchema, FilterOptions, 
+    schema::{ CreateTaskSchema, FilterOptions, UpdateTaskSchema, 
         //UpdateTaskSchema 
     },
     AppState,
@@ -10,8 +10,9 @@ use actix_web::{ get, post, web::{
     Json,
     scope,
     Query,
+    Path,
     ServiceConfig
-}, HttpResponse, Responder };
+}, HttpResponse, Responder, patch };
 use serde_json::json;
 
 
@@ -51,6 +52,7 @@ async fn create_task(
             return HttpResponse::InternalServerError()
                 .json(serde_json::json!({"status": "error","message": format!("{:?}", error)}));
         }
+    
     }
 
 }
@@ -63,7 +65,7 @@ pub async fn get_all_tasks(
     let limit = opts.limit.unwrap_or(10);
     let offset = (opts.page.unwrap_or(1) - 1) * limit;
 
-    let query_result = sqlx::query_as!(
+    match sqlx::query_as!(
         TaskModel,
         "SELECT * FROM tasks ORDER by id LIMIT $1 OFFSET $2",
         limit as i32,
@@ -71,32 +73,119 @@ pub async fn get_all_tasks(
         
     )
     .fetch_all(&data.db)
+    .await {
+        
+        Ok(tasks) => {
+        
+
+            let json_response = serde_json::json!({
+                "status": "success",
+                "result": tasks.len(),
+                "tasks": tasks
+            });
+
+            HttpResponse::Ok().json(json_response)
+
+        }
+        Err(error) => {
+            return HttpResponse::InternalServerError()
+                .json(serde_json::json!({"status": "error","message": format!("{:?}", error)}));
+        }
+ 
+    }
+}
+
+
+#[get("/tasks/{id}")]
+async fn get_task_by_id(
+    path: Path<uuid::Uuid>,
+    data: Data<AppState>,
+) -> impl Responder {
+    let task_id = path.into_inner();
+    
+    let query_result = sqlx::query_as!(TaskModel,
+    "SELECT * FROM tasks WHERE id = $1", task_id
+    )
+    .fetch_one(&data.db)
     .await;
+      
 
+    match query_result {
+        Ok(task) => {
+            let note_response = serde_json::json!({
+                "status": "success",
+                "data": serde_json::json!({
+                    "task": task
+                })
+            });
 
-    if query_result.is_err() {
-        let error_message = "Something bad happened while fetching all note tasks";
+            HttpResponse::Ok().json(note_response)
+        } 
+        Err(error) => {
+            return HttpResponse::InternalServerError()
+                .json(serde_json::json!({"status": "error","message": format!("{:?}", error)}));
+        }
 
-        return HttpResponse::InternalServerError()
-            .json(json!({ "status": "error", "message": error_message }));
     }
 
-    let tasks = query_result.unwrap();
-
-    let json_response = serde_json::json!({
-        "status": "success",
-        "result": tasks.len(),
-        "tasks": tasks
-    });
-
-    HttpResponse::Ok().json(json_response)
 }
+
+#[patch("/tasks/{id}")]
+async fn edit_task_by_id(
+    path: Path<uuid::Uuid>,
+    body: Json<UpdateTaskSchema>,
+    data: Data<AppState>,
+) -> impl Responder {
+    let task_id = path.into_inner();
+    
+   match sqlx::query_as!(TaskModel, 
+        "SELECT * FROM tasks WHERE id = $1", task_id)
+        .fetch_one(&data.db)
+        .await {
+
+        Ok(task) => {
+           match sqlx::query_as!(TaskModel,
+                "UPDATE tasks SET title = $1, content = $2 WHERE id = $3 RETURNING *", 
+                body.title.to_owned().unwrap_or(task.title),
+                body.title.to_owned().unwrap_or(task.content),
+                task_id    
+            ).fetch_one(&data.db)
+             .await {
+                Ok(task) => {
+                    let task_response = serde_json::json!({"status": "success","data": serde_json::json!({
+                        "task": task
+                    })});
+
+                    return HttpResponse::Ok().json(task_response);
+                }
+                Err(err) => {
+                    let message = format!("Error: {:?}", err);
+                    return HttpResponse::InternalServerError()
+                        .json(serde_json::json!({
+                            "status": "error",
+                            "message": message
+                        }));
+                }
+            }
+        }
+        Err(err) => {
+            let message = format!("Internal server error: {:?}", err);
+            return HttpResponse::NotFound()
+            .json(serde_json::json!({"status": "fail","message": message}));
+        }
+    }
+
+}
+ 
+
 
 pub fn config(conf: &mut ServiceConfig) {
     let scope = scope("/api")
         .service(health_checker)
         .service(create_task)
-        .service(get_all_tasks);
+        .service(get_all_tasks)
+        .service(get_task_by_id)
+        .service(edit_task_by_id);
 
     conf.service(scope);
 }
