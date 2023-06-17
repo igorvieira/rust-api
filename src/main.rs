577a1de256c8ecf83d6004da31a973e3641a28e7
Igor Vieira <igor.p.r.vieira@gmail.com>
2023-06-17 18:43:18 -0300

    chore: create task

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,46 @@ mod model;
 mod services;
 
 use actix_web::{
+    web,
     App,
     HttpServer
 };
+
+use dotenv::dotenv;
+
+use sqlx::{
+    Postgres,
+    Pool,
+    postgres::PgPoolOptions
+};
+
+pub struct AppState {
+    db: Pool<Postgres>
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server started successfully");
 
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must to be set");
+    let pool = match PgPoolOptions::new().max_connections(10).connect(&database_url).await {
+        Ok(pool) => {
+            println!("Connetion DB resolved");
+            pool
+        }
+        Err(error) => {
+            println!("Failed to connect to the dabase: {:?}", error);
+            std::process::exit(1)
+        }
+    };
+
+
+
     HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(AppState { db: pool.clone()}))
             .configure(services::config)
     })
     .bind(("127.0.0.1", 8080))?
--- src/services.rs
+++ src/services.rs
@@ -1,14 +1,19 @@
 use actix_web::{
     web::{
         scope,
+        Json,
+        Data,
         ServiceConfig
     },
     get,
+    post,
     HttpResponse,
     Responder
 };
 
 use serde_json::json;
+
+use crate::{schema::CreateTaskSchema, model::TaskModel, AppState};
 
 
 #[get("/healthchecker")]
@@ -21,9 +26,50 @@ async fn health_checker() -> impl Responder {
     }))
 }
 
+#[post("/task")]
+async fn create_task(
+    body: Json<CreateTaskSchema>,
+    data: Data<AppState>
+) -> impl Responder {
+
+    match 
+        sqlx::query_as!(
+            TaskModel,
+            "INSERT INTO tasks (title, content) VALUES ($1, $2)
+            RETURNING * ",
+            body.title.to_string(),
+            body.content.to_string()
+        )
+        .fetch_one(&data.db)
+        .await {
+            Ok(task) => {
+                let note_response = json!({
+                    "status": "success",
+                    "task": json!({
+                        "task": task,
+                    })
+                });
+
+                return HttpResponse::Ok().json(note_response);
+            }
+            Err(error) => {
+
+                return HttpResponse::InternalServerError().json(
+                    json!({
+                        "status": "error",
+                        "message": format!("{:?}", error)
+                    })
+                )
+            }
+        }
+
+}
+
 
 pub fn config(conf:  &mut ServiceConfig) {
-    let scope = scope("/api").service(health_checker);
+    let scope = scope("/api")
+            .service(health_checker)
+            .service(create_task);
 
 
     conf.service(scope);
