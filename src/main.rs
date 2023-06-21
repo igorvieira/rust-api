36d45546aceec55f4f27ab8a5dce8251b1cf77db
Igor Vieira <igor.p.r.vieira@gmail.com>
2023-06-20 21:24:20 -0300

    chore: logs with Logger

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod services;
 use actix_web::{
     web,
     App,
-    HttpServer
+    HttpServer,
+    middleware::Logger
 };
 
 use dotenv::dotenv;
@@ -24,7 +25,12 @@ pub struct AppState {
 async fn main() -> std::io::Result<()> {
     println!("Server started successfully");
 
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info")
+    }
+
     dotenv().ok();
+    env_logger::init();
 
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must to be set");
     let pool = match PgPoolOptions::new().max_connections(10).connect(&database_url).await {
@@ -44,7 +50,9 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(AppState { db: pool.clone()}))
             .configure(services::config)
+            .wrap(Logger::default())
     })
     .bind(("127.0.0.1", 8080))?
     .run().await
 }
+
